80a0bd7402d62998d0fa0e81cd50953f7991bf6c
Mike D. Saint <douglas-mike1@hotmail.com>
2022-08-16 21:38:16 -0300

    .

---
+++ gestaoMidiaAPI/gestaoMidiaAPI/Domain/Data/DomainObjects/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace gestaoMidiaAPI.Domain.Data.DomainObjects
+{
+    public abstract class Entity
+    {
+        public virtual Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ gestaoMidiaAPI/gestaoMidiaAPI/Services/IService/IBaseService.cs
@@ -0,0 +1,19 @@
+﻿using AngleSharp.Dom;
+using FluentValidation;
+using System;
+
+namespace gestaoMidiaAPI.Services
+{
+    public interface IBaseService<TEntity> : IDisposable<TEntity> where TEntity : Entity
+    {
+        TEntity Add<TValidator>(TEntity entity) where TValidator : AbstractValidator<TEntity>;
+
+        void Delete(TEntity entity);
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(Guid id);
+
+        TEntity Update<TValidator>(TEntity entity) where TValidator : AbstractValidator<TEntity>;
+    }
+}
